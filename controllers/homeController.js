038f7c8acb71580dd93fcb6927a09b456a39e008
joan-kii <joan.sb81@gmail.com>
2021-10-13 05:27:22 +0200

    añade render messages en home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,10 @@
+const Message = require('../models/messageModel');
+
 exports.home_page = function(req, res, next) {
-  res.render('index', {currentUser: res.locals.currentUser});
+  Message.find()
+         .sort([['createdAt', 'descending']])
+         .exec(function(err, messages_list) {
+           if (err) return next(err);
+           res.render('index', {messages: messages_list});
+         })
 };
