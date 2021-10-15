11613c0a9227aeb6848e5ab7ed89e0bd0e344f14
joan-kii <joan.sb81@gmail.com>
2021-10-15 18:05:53 +0200

    soluciona bug signup

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -10,10 +10,12 @@ exports.message_create_post = [
     body('title', 'Title Required')
         .trim()
         .isLength({min: 1})
+        .withMessage('Enter the title.')
         .escape(),
     body('mesage', 'Message Required')
         .trim()
         .isLength({min: 1})
+        .withMessage('Enter the message.')
         .escape(),
     (req, res, next) => {
       const errors = validationResult(req);
--- controllers/userController.js
+++ controllers/userController.js
@@ -15,45 +15,86 @@ exports.user_create_post = [
   body('firstName', 'First Name Required')
       .trim()
       .isLength({min: 1})
+      .withMessage('Enter your first name.')
+      .isAlphanumeric()
+      .withMessage('Enter only alphanumeric characters in the first name')
       .escape(),
   body('secondName', 'Second Name Required')
       .trim()
       .isLength({min: 1})
+      .withMessage('Enter your second name.')
+      .isAlphanumeric()
+      .withMessage('Enter only alphanumeric characters in the second name')
       .escape(),
   body('userName', 'User Name Required')
       .trim()
       .isLength({min: 1})
-      .escape(),
+      .withMessage('Enter your username.')
+      .escape()
+      .isAlphanumeric()
+      .withMessage('Enter only alphanumeric characters in username')
+      .custom((value) => {
+        User.findOne({ userName: value }, function(err, user) {
+          if (err) throw new Error(err);
+          if (user) {
+            throw new Error('There is a user with this username.');
+          } else {
+            return true;
+          }
+        })
+      }),
   body('password', 'Password Required')
       .trim()
-      .isLength({min: 1})
-      .escape(),
+      .isLength({min: 4})
+      .withMessage('Enter your password, at least 4 characters.')
+      .escape()
+      .isAlphanumeric()
+      .withMessage('Enter only alphanumeric characters in the password'),
   body('confirmPassword', 'Confirm Password Required')
       .trim()
-      .isLength({min: 1})
+      .isLength({min: 4})
+      .withMessage('Confirm your password.')
       .escape()
-      .custom((value, { req }) => value === req.body.password),
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Password confirmation does not match password');
+        } else {
+          return true;
+        }
+      }),
   body('passcode')
       .trim()
-      .isLength({min: 1})
-      .escape(),
+      .withMessage('Enter the admin passcode.')
+      .escape()
+      .isAlphanumeric()
+      .withMessage('Enter only alphanumeric characters in the passcode.')
+      .custom((value) => {
+        if (value !== '' && value !== process.env.ADMIN_PASSCODE) {
+          throw new Error('Admin passcode incorrect');
+        } else {
+          return true;
+        }
+      }),
   (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-      if (err) return next(err);
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) res.render('signup-form', {error: errors.array()})
-      const user = new User({
-        firstName: req.body.firstName,
-        secondName: req.body.secondName,
-        userName: req.body.userName,
-        password: hashedPassword,
-        status: false,
-        isAdmin: req.body.passcode === process.env.ADMIN_PASSCODE ? true : false
-        }).save(err => {
-          if (err) res.render('signup-form', {error: err});
-          res.redirect('/');
-      });
-    })
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('signup-form', { error: errors.array({onlyFirstError: true}) });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+        const user = new User({
+          firstName: req.body.firstName,
+          secondName: req.body.secondName,
+          userName: req.body.userName,
+          password: hashedPassword,
+          status: false,
+          isAdmin: req.body.passcode === process.env.ADMIN_PASSCODE ? true : false
+          }).save(err => {
+            if (err) res.render('signup-form', {error: err});
+            res.redirect('login-form');
+        });
+      })
+    }
   }
 ];
 
@@ -63,10 +104,24 @@ exports.user_login_get = function(req, res, next) {
   res.render('login-form');
 };
 
-exports.user_login_post = passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-});
+exports.user_login_post = [
+  body('userName', 'Username required.')
+      .trim()
+      .isLength({min: 1})
+      .withMessage('Enter a username.')
+      .escape(),
+  body('password', 'Password required.')
+      .trim()
+      .isLength({min: 1})
+      .withMessage('Enter a password.')
+      .escape(),
+
+      // Seguir aqui
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  });
+];
 
 // Log Out User
 
@@ -85,8 +140,11 @@ exports.user_membership_post = [
   body('accessKey', 'Access Key required')
   .trim()
   .isLength({min: 1})
+  .withMessage('Enter the access key.')
   .escape()
-  .custom((value, { req }) => value === process.env.MEMBERSHIP_KEY),
+  .isAlphanumeric()
+  .withMessage('Enter only alphanumeric characters in the access key.')
+  .custom((value) => value === process.env.MEMBERSHIP_KEY),
   (req, res, next) => {
     let id = mongoose.Types.ObjectId(res.locals.currentUser._id);
     User.findByIdAndUpdate({ _id: id }, { status: true }, function(err, result) {
